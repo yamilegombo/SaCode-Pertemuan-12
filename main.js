5012ebd17be9380f1f65e9c2fdaa80fb4ee6fee8
Mile Gombo <yamilegombo30@gmail.com>
2024-07-02 19:45:37 +0900

    commit pertama

---
+++ main.js
@@ -0,0 +1,7 @@
+// method menambahkan teks
+//let hello = document.getElementById("demo").textContent = "Hello Word";
+
+//method tambahkan teks dan tag
+const create = document.createElement("p");
+//create.textContent = "Hello Yamile";
+document.body.appendChild(create);
